9146897ebc82540b373067021157f2d2938f0820
Marah-uwase <maranahuwase12@gmail.com>
2021-01-11 12:52:41 +0200

    creating the models for the gallery app

--- myGallery/settings.py
+++ myGallery/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'gallery',
-    'bootstrap3',
+    # 'bootstrap3',
+    # 'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
