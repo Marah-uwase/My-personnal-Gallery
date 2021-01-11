2b4bfbb8bb07910ce7c5cc93ef65cee06047cfe1
Marah-uwase <maranahuwase12@gmail.com>
2021-01-11 13:29:24 +0200

    adding the category model

--- gallery/models.py
+++ gallery/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import datetime as dt
 
 # Create your models here.
-class Locatio(models.Model):
+class Location(models.Model):
     country = models.CharField(max_length =30)
     city = models.CharField(max_length =30)
     def __str__(self):
