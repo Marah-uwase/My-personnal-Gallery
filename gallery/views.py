0edd9750bb70eed589d45253ea8bbcadf99aa9f1
Marah-uwase <maranahuwase12@gmail.com>
2021-01-11 12:57:02 +0200

    adding the error handling

--- gallery/views.py
+++ gallery/views.py
@@ -27,7 +27,7 @@ def search_results(request):
     args:
     order defines category
     '''
-    if 'image' in request.GET and request.GET["image"]:
+    if 'image' in request.GET and request.GET["image"]
         search_term = request.GET.get("image")
         searched_images = Image.search_by_category(search_term)
         message = f"{search_term}"
