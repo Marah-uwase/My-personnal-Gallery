a9262a3639cd97dc95647ca388061786fde3befd
Marah-uwase <maranahuwase12@gmail.com>
2021-01-11 08:33:21 +0200

    adding the bootstrap into the installed apps

--- myGallery/settings.py
+++ myGallery/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'gallery',
-    # 'bootstrap4',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
