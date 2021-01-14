5225aa9b6e117e847a2b6be0c0ae075340fe3282
Marah-uwase <maranahuwase12@gmail.com>
2021-01-14 21:44:59 +0200

    correcting the indentation error

--- myGallery/settings.py
+++ myGallery/settings.py
@@ -11,11 +11,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import django_heroku
 import dj_database_url
-from decouple import config,Csv
+# from decouple import config,Csv
 from pathlib import Path
-
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -82,21 +80,18 @@ WSGI_APPLICATION = 'myGallery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         'USER': 'maranatha',
-#     'PASSWORD':'marah31998',
-#     }
-# }
-
 DATABASES = {
-       'default': dj_database_url.config(
-           default=config('DATABASE_URL')
-       )
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'maranatha',
+    'PASSWORD':'marah31998',
+    }
+}
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
